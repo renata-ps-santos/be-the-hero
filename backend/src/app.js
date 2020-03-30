3b0dcb2c863704448b80a75c008fc69649beaa3b
Renata <renataps.360@gmail.com>
2020-03-29 21:18:40 -0300

    tests apis

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { errors } = require('celebrate');
 const cors = require('cors');
 const routes = require('./routes');
 
@@ -7,5 +8,6 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(routes);
+app.use(errors());
 
-app.listen(3333);
+module.exports = app; 
--- backend/src/controllers/OngController.js
+++ backend/src/controllers/OngController.js
@@ -1,6 +1,5 @@
-const crypto = require('crypto');
 const connection = require('../database/connection');
-
+const generateUniqueId = require('../utils/generateUniqueId');
 module.exports = {
   async getlist(request, response) {
     const ongs = await connection('ongs').select('*');
@@ -9,8 +8,7 @@ module.exports = {
   },
   async create(request, response) {
     const { name, email, whatsapp, city, uf } = request.body;
-    const id = crypto.randomBytes(4).toString('HEX');
-
+    const id = generateUniqueId();
     await connection('ongs').insert({
       id,
       name,
